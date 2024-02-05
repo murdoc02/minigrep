e7d1d4df7653b8e8395681ae990910d10f2fb91d
rg <garzon.r@gmail.com>
2024-02-04 20:58:02 -0500

    adding tests

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+        let query = args[0].clone();
+        let file_path = args[1].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    return vec![];
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick Three.";
+
+        assert_eq!(vec!["safe, fast ,productive."], search(query, contents));
+    }
+}
