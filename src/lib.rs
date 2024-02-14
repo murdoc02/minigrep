35f4d2c4ce738e21c1e68281f89e891266199e1c
rg <garzon.r@gmail.com>
2024-02-13 22:39:31 -0500

    swapped in iterators for arg parsing and search

--- src/lib.rs
+++ src/lib.rs
@@ -9,26 +9,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        let mut ignore_case = false;
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let ignore_env_result = env::var("IGNORE_CASE");
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a query string"),
+        };
 
-        if !ignore_env_result.is_ok() {
-            if args.iter().any(|i| i == "-i") {
-                ignore_case = true;
-            }
-        } else {
-            let ignore_case_env_val = ignore_env_result.unwrap();
-            if ignore_case_env_val != "1" {
-                ignore_case = true;
-            }
-        }
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query,
@@ -55,14 +49,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    return results;
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
